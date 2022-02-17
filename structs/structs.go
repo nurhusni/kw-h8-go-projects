b3c742506588cd39b68b5cfe2cabc69e7c8ae14d
Panji Ahmad Nurhusni <panjinurhusni@gmail.com>
2022-02-17 16:55:50 +0700

    Added changes to structs

--- structs/structs.go
+++ structs/structs.go
@@ -8,16 +8,16 @@ import (
 
 type Items struct {
 	gorm.Model
-	ItemId      int
-	ItemCode    int
+	ItemId      uint
+	ItemCode    uint
 	Description string
-	Quantity    int
+	Quantity    uint
 	OrderId     Orders `gorm:"foreignkey:OrderId"`
 }
 
 type Orders struct {
 	gorm.Model
-	OrderId      int
+	OrderId      uint
 	CustomerName string
 	OrderedAt    time.Time
 	Items        []Items
