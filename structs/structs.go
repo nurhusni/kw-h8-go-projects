6e3903dacb618dbaf30c072a59676e1b585255cb
Panji Ahmad Nurhusni <panjinurhusni@gmail.com>
2022-02-18 22:10:42 +0700

    Added changes to structs

--- structs/structs.go
+++ structs/structs.go
@@ -9,16 +9,13 @@ import (
 
 type Order struct {
 	gorm.Model
-	// OrderID      uint `gorm:"primaryKey;autoIncrement:true"`
 	CustomerName string
 	OrderedAt    time.Time
 	Item         Item `gorm:"foreignKey:OrderID;references:ID"`
-	// Item Item `gorm:"foreignKey:OrderID;references:OrderID"`
 }
 
 type Item struct {
 	gorm.Model
-	// ItemID      uint `gorm:"primaryKey;autoIncrement:true"`
 	ItemCode    int64
 	Description string
 	Quantity    int64
