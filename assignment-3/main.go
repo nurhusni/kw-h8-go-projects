4fb6b5bfe3c84477787c9303700db81c7fc93c4c
nurhusni <panjinurhusni@gmail.com>
2022-02-24 18:07:31 +0700

    Removed unnecesasry init function

--- assignment-3/main.go
+++ assignment-3/main.go
@@ -8,11 +8,6 @@ import (
 
 var PORT = ":3000"
 
-func init() {
-	values := controllers.GenerateStatus()
-	_ = values
-}
-
 func main() {
 	http.HandleFunc("/", controllers.ShowStatus)
 	http.ListenAndServe(PORT, nil)
