61f7323538b02da199b9c0188093e278681e4d81
nurhusni <panjinurhusni@gmail.com>
2022-03-01 01:52:03 +0700

    Added validators

--- final-project/go.mod
+++ final-project/go.mod
@@ -3,9 +3,9 @@ module khg-final-project
 go 1.17
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.7
-	github.com/go-playground/validator/v10 v10.10.0
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
 	gorm.io/driver/postgres v1.3.1
 	gorm.io/gorm v1.23.1
@@ -15,6 +15,7 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
 	github.com/golang/protobuf v1.3.3 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.10.1 // indirect
--- final-project/models/user.go
+++ final-project/models/user.go
@@ -1,12 +1,29 @@
 package models
 
+import (
+	"github.com/asaskevich/govalidator"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	GormModel
-	Username     string        `gorm:"not null" json:"username"`
-	Email        string        `gorm:"not null" json:"email"`
-	Password     string        `gorm:"not null" json:"password"`
-	Age          uint          `gorm:"not null" json:"age"`
+	Username     string        `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Username is required"`
+	Email        string        `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Email is required,email~Invalid email format"`
+	Password     string        `gorm:"not null" json:"password" form:"password" valid:"required~Password is required,minstringlength(6)~Password has to have a minimum length of 6 characters"`
+	Age          uint          `gorm:"not null" json:"age" form:"age" valid:"requred~Age is required,min=8~Minimum age is 8 years old"`
 	Photos       []Photo       `json:"-"`
 	Comments     []Comment     `json:"-"`
 	SocialMedias []SocialMedia `json:"-"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	_, errCreate := govalidator.ValidateStruct(u)
+
+	if errCreate != nil {
+		err = errCreate
+		return
+	}
+
+	err = nil
+	return
 }
